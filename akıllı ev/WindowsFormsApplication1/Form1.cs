dba6bd43ee2f4f9b308b9cc56161dd8568a25f41
Yiğit Oğuz <ogsano79@gmail.com>
2025-12-22 13:59:20 +0300

    Add files via upload

---
+++ akıllı ev/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+            listBox1.Items.Add("lambalar açık");
+
+            }
+            else
+            {
+            listBox1.Items.Add("lambalar kapalı");
+            }
+            if (checkBox2.Checked == true)
+            {
+            listBox1.Items.Add("kombi açık");
+            }  
+            else 
+            {
+            listBox1.Items.Add("kombi kapalı");
+            }
+
+        }
+    }
+}
