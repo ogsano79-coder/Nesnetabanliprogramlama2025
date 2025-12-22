8ed08df8294005587f064098bd65cc936f83a019
Yiğit Oğuz <ogsano79@gmail.com>
2025-12-22 13:50:40 +0300

    Add files via upload

---
+++ tuş takımı/4gün/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4gün
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //textBox1.ForeColor = Color.Yellow;
+            //textBox1.BackColor = Color.Blue;
+            //textBox1.Font = new Font("Adobe Arabic", 10);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label1.Text = "1";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text = "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text = "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+        }
+    }
+}
