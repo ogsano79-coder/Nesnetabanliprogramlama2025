2ec12535f54f5a625f9f4570b1244c20caca4473
Yiğit Oğuz <ogsano79@gmail.com>
2025-12-22 13:45:02 +0300

    Add files via upload

---
+++ bir boyutlu diziler uyg1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimler = new string[5];
+        int indexno = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isimler[indexno] = textBox1.Text;
+            indexno++;
+        }
+    }
+}
