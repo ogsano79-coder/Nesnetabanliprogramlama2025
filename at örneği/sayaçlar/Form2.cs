fc4e99b4d6782fe90020a4fdbcdc21dc296ecb2c
Yiğit Oğuz <ogsano79@gmail.com>
2025-12-23 11:36:21 +0300

    Add files via upload

---
+++ at örneği/sayaçlar/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayaçlar
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        int sayı;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            sayı = Convert.ToInt32(textBox1.Text);
+            sayı = sayı + 1;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayı = Convert.ToInt32(textBox1.Text);
+            sayı = sayı - 1;
+        }
+    }
+}
