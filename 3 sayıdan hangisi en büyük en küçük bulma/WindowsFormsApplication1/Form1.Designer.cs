f21e3dcd493c5596883ce0a08120c3a54078a5ea
Yiğit Oğuz <ogsano79@gmail.com>
2025-12-23 11:36:54 +0300

    Add files via upload

---
+++ 3 sayıdan hangisi en büyük en küçük bulma/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label7 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Location = new System.Drawing.Point(18, 84);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(33, 13);
+            this.Label7.TabIndex = 21;
+            this.Label7.Text = "sayıı3";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(18, 57);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(31, 13);
+            this.Label6.TabIndex = 20;
+            this.Label6.Text = "sayı2";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(20, 26);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(29, 13);
+            this.Label3.TabIndex = 17;
+            this.Label3.Text = "say1";
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(57, 104);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(100, 23);
+            this.Button1.TabIndex = 14;
+            this.Button1.Text = "hesapla";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Location = new System.Drawing.Point(57, 77);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(100, 20);
+            this.TextBox3.TabIndex = 13;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(57, 50);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(100, 20);
+            this.TextBox2.TabIndex = 12;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(57, 23);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(100, 20);
+            this.TextBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(189, 143);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.TextBox3);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.TextBox TextBox3;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.TextBox TextBox1;
+    }
+}
+
---
+++ 3 sayıdan hangisi en büyük en küçük bulma/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int sayı1,sayı2,sayı3;
+            sayı1 = Convert.ToInt32(TextBox1.Text);
+            sayı2 = Convert.ToInt32(TextBox2.Text);
+            sayı3 = Convert.ToInt32(TextBox3.Text);
+            
+            if (sayı1>sayı2 && sayı1>sayı3)
+
+            {
+                MessageBox.Show("sayı1 en büyük");
+            }
+
+            else if (sayı1 < sayı2 && sayı1 < sayı3)
+            {
+                MessageBox.Show("sayı1 en küçük");
+            }
+
+            else if (sayı2 > sayı1 && sayı2 > sayı3)
+            {
+                MessageBox.Show("sayı2 en büyük");
+            }
+
+            else if (sayı2 < sayı1 && sayı2 < sayı3)
+            {
+                MessageBox.Show("sayı2 en küçük");
+            }
+            else if (sayı3 > sayı1 && sayı3 > sayı2)
+
+            {
+                MessageBox.Show("sayı3 en büyük");
+            }
+            else if (sayı3 < sayı1 && sayı3 < sayı2)
+
+            {
+                MessageBox.Show("sayı3 en küçük");
+            }
+            else
+            {
+                MessageBox.Show("yanlış karakter");
+            }
+        }
+    }
+}
